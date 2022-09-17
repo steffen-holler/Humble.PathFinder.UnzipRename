b62fd6968f82fdc8f3f65dbc116864860990f40c
Steffen Delwyn Holler <sd_holler@hotmail.com>
2022-09-17 12:35:19 -0500

    working document parsing

--- Humble.PathFinder.UnzipRename/Document.cs
+++ Humble.PathFinder.UnzipRename/Document.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Humble.PathFinder.UnzipRename
@@ -13,36 +13,100 @@ namespace Humble.PathFinder.UnzipRename
     internal class Document
     {
         private string baseName = "";
-        private string fileName = "";
 
         /// <summary>
         /// Gets the original name of the file assigned by Paizo. 
         /// </summary>
-        public string OringalName { get; init; } = "";
+        public string OringalName { get; private set; } = "";
         
         /// <summary>
         /// Gets the more human readable name for the file.
         /// </summary>
         public string NewName 
         {
-            get { return fileName; }
-            internal set
+            get
             {
-                // todo: parse file name
-                fileName = value;
+                return baseName + ParseFileName(OringalName);
             }
         } 
 
-        public Document(string orignialName, string zipFolderName)
+        public Document(string fileName, string zipFolderName)
         {
-            OringalName = orignialName;
+            OringalName = fileName;
             baseName = ParseFolderName(zipFolderName);
-
         }
 
         internal static string ParseFolderName(string folderName)
         {
-            return folderName;
+            if (string.IsNullOrWhiteSpace(folderName))
+                return string.Empty;
+
+            folderName = folderName.Replace("-SingleFile", "");
+            folderName = folderName.Replace("-FilePerChapter", "");
+            folderName = folderName.Replace("-OneFilePerChapter", "");
+            folderName = folderName.Replace("PDF", "");
+
+            string replacement = " ";
+            var spaceOutRegEx = new Regex("(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|(?<=[0-9])(?=[A-Z][a-z])|(?<=[a-zA-Z])(?=[0-9])");
+            string splitFolderName = spaceOutRegEx.Replace(folderName, replacement);
+
+            if (Regex.IsMatch(splitFolderName, @"\d"))
+            {
+                // find the first set of number and delete the rest. 
+                string[] name = splitFolderName.Split(' ');
+                splitFolderName = "";
+                foreach(var word in name)
+                {
+                    splitFolderName += word + " ";
+                    if (Regex.IsMatch(word, @"\d"))
+                        break;
+
+                }
+            }
+            return splitFolderName.Trim();
+        }
+
+        internal static string ParseFileName(string fileName)
+        {
+            string ext = fileName.Substring(fileName.LastIndexOf("."));
+            fileName = fileName.Replace(ext, "");
+            fileName = fileName.Replace("-2nd", "");
+            fileName = fileName.Replace("-3rd", "");
+            fileName = fileName.Replace(" Printing", "");
+
+            string[] names = fileName.Split(' ');
+            fileName = "";
+            foreach (string name in names)
+            {
+                if (name.StartsWith("PZO"))
+                    continue;
+                if (Regex.IsMatch(name, @"\d"))
+                {
+                    if (name.Contains("-"))
+                    {
+                        string[] numbers = name.Split('-');
+                        string pageNumbers = "";
+                        for(int i = 0; i < numbers.Length; i++)
+                        {
+                            pageNumbers += int.Parse(numbers[i]).ToString() + "-";
+                        }
+                        pageNumbers = pageNumbers.Substring(0, pageNumbers.LastIndexOf("-"));
+                        fileName += "p" + pageNumbers + " ";
+                    }
+                    else
+                    {
+                        int pageNumber = int.Parse(name);
+                        fileName += "p" + pageNumber.ToString() + " ";
+                    }
+                }
+
+                else 
+                    fileName += name + " ";
+            }
+            
+
+
+            return fileName.Trim() + ext;
         }
     }
 }
--- Humble.PathFiner.UnzipRename.Test/TestDocument.cs
+++ Humble.PathFiner.UnzipRename.Test/TestDocument.cs
@@ -1,16 +1,48 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Humble.PathFiner.UnzipRename;
+using Humble.PathFinder.UnzipRename;
 
-namespace Humble.PathFiner.UnzipRename.Test
+namespace Humble.PathFinder.UnzipRename.Test
 {
     [TestClass]
     public class TestDocument
     {
         [TestMethod]
-        public void TestFolderNameParsing(string FolderName)
+        public void TestFolderNameParsing()
         {
-            
+            var FolderName = "PathfinderAdventurePath174ShadowsOfTheAncientsStrengthOfThousands6Of6PDF-SingleFile";
+            var newName = Document.ParseFolderName(FolderName);
+            Assert.AreEqual(newName, "Pathfinder Adventure Path 174");
+
+            FolderName = "PathfinderAdvancedPlayersGuidePDF-FilePerChapter";
+            newName = Document.ParseFolderName(FolderName);
+            Assert.AreEqual(newName, "Pathfinder Advanced Players Guide");
+
+            FolderName = "PathfinderAdvancedPlayersGuidePDF-SingleFile";
+            newName = Document.ParseFolderName(FolderName);
+            Assert.AreEqual(newName, "Pathfinder Advanced Players Guide");
         }
+
+        [TestMethod]
+        public void TestFileNameParsing()
+        {
+            var fileName = "PZO2105 APG 000 Cover-2nd Printing.pdf";
+            var parseNaem = Document.ParseFileName(fileName);
+            Assert.AreEqual(parseNaem, "APG p0 Cover.pdf");
+
+            fileName = "PZO2105 APG 194-199-2nd Printing.pdf";
+            parseNaem = Document.ParseFileName(fileName);
+            Assert.AreEqual(parseNaem, "APG p194-199.pdf");
+
+            fileName = "PZO90169E.pdf";
+            parseNaem = Document.ParseFileName(fileName);
+            Assert.AreEqual(parseNaem, ".pdf");
+
+            fileName = "PZO90169E Interactive Maps.pdf";
+            parseNaem = Document.ParseFileName(fileName);
+            Assert.AreEqual(parseNaem, "Interactive Maps.pdf");
+        }
+
+
 
         [TestMethod]
         [DataRow("PZO90174E.pdf", "PathfinderAdventurePath174ShadowsOfTheAncientsStrengthOfThousands6Of6PDF-SingleFile")]
